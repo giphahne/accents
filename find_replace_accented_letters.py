ba2a44643103237c3b171d57da00c7c2862dc69f
Dan Hahne <contact@danhahne.com>
2021-02-02 23:10:48 -0500

    updates

--- find_replace_accented_letters.py
+++ find_replace_accented_letters.py
@@ -6,6 +6,25 @@ from textgrid import Interval
 
 
 def clean_mark(mark):
+    #             """
+    #             replace each accented letter with a capital letter
+    #             use the same values as the original interval
+    #             except that each accented letter with be
+    #             replaced by a capital letter
+    #             """
+    #             letter.replace("U+00E1", "A")
+    #             letter.replace("U+00E9", "E")
+    #             letter.replace("U+00ED", "I")
+    #             letter.replace("U+00F3", "O")
+    #             letter.replace("U+00FA", "U")
+    #             letter.replace("U+00C1", "A")
+    #             letter.replace("U+00C9", "E")
+    #             letter.replace("U+00CD", "I")
+    #             letter.replace("U+00D3", "O")
+    #             letter.replace("U+00DA", "U")
+    #             letter.replace("U+00F1", "N")
+    #             letter.replace("U+00D1", "N")
+
     return mark.upper()
 
 
@@ -33,33 +52,6 @@ def remove_accents(source_tg):
     for tier in source_tg.getNames():
         dest_tg.tiers.append(clean_tier(source_tg.getFirst(tier)))
 
-    # for tier in textgrid:
-    #     for interval in tier:
-    #         for letter in interval:
-    #             """
-    #             replace each accented letter with a capital letter
-    #             use the same values as the original interval
-    #             except that each accented letter with be
-    #             replaced by a capital letter
-    #             """
-    #             letter.replace("U+00E1", "A")
-    #             letter.replace("U+00E9", "E")
-    #             letter.replace("U+00ED", "I")
-    #             letter.replace("U+00F3", "O")
-    #             letter.replace("U+00FA", "U")
-    #             letter.replace("U+00C1", "A")
-    #             letter.replace("U+00C9", "E")
-    #             letter.replace("U+00CD", "I")
-    #             letter.replace("U+00D3", "O")
-    #             letter.replace("U+00DA", "U")
-    #             letter.replace("U+00F1", "N")
-    #             letter.replace("U+00D1", "N")
-    #     #once each interval has been cleaned
-    #     #return the cleaned product!
-
-    # # does this need to be here since it would just work by
-    # # going into the directory and changing everything
-    # # within the directory
     return dest_tg
 
 
